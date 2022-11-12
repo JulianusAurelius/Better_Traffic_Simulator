ee93b738a35bd539f34c4af884bdaf86d3563eb3
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-11-12 14:33:15 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -3,8 +3,35 @@
 
 #define SEGMENT_WIDTH 1
 
+
+
 // global variable
 unsigned long long int penalty = 0;
+
+int*** genIntersection(int streetOne, int streetTwo, int streetThree, int streetFour) {
+    int*** intersection = (int**)malloc(sizeof(int**) * 4);
+    intersection[0] = (int*)calloc(streetOne, sizeof(int*));
+    intersection[1] = (int*)calloc(streetTwo, sizeof(int));
+    intersection[2] = (int*)calloc(streetThree, sizeof(int));
+    intersection[3] = (int*)calloc(streetFour, sizeof(int));
+
+    int i;
+    for (i=0; i<streetOne; i++) {
+        // is_car, speed, acceleration, turn_left, turn_right, turn_straight, emergency_vehicle, time_waiting
+        intersection[0][i] = (int*)calloc(8, sizeof(int));
+    }
+    for (i=0; i<streetTwo; i++) {
+        intersection[1][i] = (int*)calloc(8, sizeof(int));
+    }
+    for (i=0; i<streetThree; i++) {
+        intersection[2][i] = (int*)calloc(8, sizeof(int));
+    }
+    for (i=0; i<streetFour; i++) {
+        intersection[3][i] = (int*)calloc(8, sizeof(int));
+    }
+
+    return intersection;
+}
 
 // function prototypes
 int** runFiveFrames(int* lightControl);
@@ -24,7 +51,7 @@ int framePenalty(int** intersection);
 int emergencyVehiclePath(int** intersection, int laneOne, int laneTwo);
 int emergencyVehiclePresent(int** intersection, int laneOne, int laneTwo);
 
-//hello world
+
 
 // intersection =
 // {
