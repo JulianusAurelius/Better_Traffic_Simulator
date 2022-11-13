1f5b8013930463b59e79faf5b3a0e09531966bc0
JulianusAurelius <MJayBeltz@gmail.com>
2022-11-13 08:58:16 -0600

    Fix minor misplacement

--- calcFollowDistance.h
+++ calcFollowDistance.h
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 #ifndef MAIN_H
-
+#include "main.h"
 #endif
 
 #ifndef FOLLOWDIS_H
 #define FOLLOWDIS_H
-#include "main.h"
 // width of a segment of road, 1 ft by default
 #define SEPARATION_TIME 5
 #endif
