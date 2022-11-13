a1aa3ac4ac962407a7abae46c56e41e0a9ec6b70
acantusoto <acs.cantu.soto@gmail.com>
2022-11-13 08:56:06 -0600

    fixed calcFollowDistance.h

--- calcFollowDistance.h
+++ calcFollowDistance.h
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 #ifndef MAIN_H
+
+#endif
+
+#ifndef FOLLOWDIS_H
+#define FOLLOWDIS_H
 #include "main.h"
 // width of a segment of road, 1 ft by default
 #define SEPARATION_TIME 5
